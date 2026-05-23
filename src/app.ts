e0bad03016e35f113507cfb74e09400204670ab1
Samir Uddin Ahamed <shahilahamed2001@gmail.com>
2026-05-24 03:41:49 +0600

    Fixed mainter access

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@
 import express, { type Application, type Request, type Response } from "express"
 import  { userRoute } from "./user/user.route"
 import { issuesRouter } from "./issue/issue.route"
+import { globalErrorHandler } from "./middleware/globalErrorHandler"
 const app:Application = express()
 
 app.use(express.json())
@@ -14,5 +15,9 @@ app.use("/api/issues",issuesRouter)
 app.get('/', (req:Request, res:Response) => {
   res.send('Hello World!')
 })
+
+
+app.use(globalErrorHandler)
+
 
 export default app;
